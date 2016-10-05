e9dfae2828603600deb45c7f9b284fda65b9fb14
Lapa <julien.lapa@ynov.com>
2016-10-05 16:03:22 +0200

    Les routes admins

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ var bodyParser = require('body-parser');
 
 var routes = require('./app/routes/index');
 var users = require('./app/routes/users');
+var admins = require('./app/routes/admins');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app/views'));
+app.set('views', path.join(__dirname, './app/views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/admins', admins);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Xnov Blog' });
 });
 
 router.get('/signup', function(req, res, next){
