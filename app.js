38ce128c61df946efad9ea7eee0c82260f9368da
Elirios-Larei <julien.lapa@ynov.com>
2016-10-06 12:04:25 +0200

    Local test

--- app.js
+++ app.js
@@ -1,67 +1,4 @@
-<<<<<<< HEAD
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var routes = require('./app/routes/index');
-var users = require('./app/routes/users');
-var admins = require('./app/routes/admins');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, './app/views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-app.use('/admins', admins);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
-=======
 "use strict";
 
 
@@ -140,4 +77,3 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
->>>>>>> 411ec110a5e47788de89266cbf84360367b25d43
